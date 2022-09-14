25b8342aba6464de2a2e2c5dcaea629bcbfb27a8
jjjjose <jjjjoseignacio@gmail.com>
2022-09-13 21:40:27 -0400

    delete console log

--- src/class/Subquest.ts
+++ src/class/Subquest.ts
@@ -17,7 +17,6 @@ class SubquestClass {
             console.log("Error:", err);
             return;
           }
-          console.log(results);
           resolve(this.convert(results));
         }
       );
