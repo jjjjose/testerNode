45ba77f8d6abbcf6cdfd3ef888ff7e90084a33b9
jjjjose <jjjjoseignacio@gmail.com>
2022-09-13 23:45:20 -0400

    scan by virus Total

---
+++ src/class/VirusTotal.ts
@@ -0,0 +1,57 @@
+const fetch = require("node-fetch");
+//apikey env
+const API_KEY = process.env.API_KEY_TOTALVIRUS;
+
+class VirusTotalClass {
+  async scan(host: string) {
+    let urlBase = `https://www.virustotal.com/api/v3/domains/${host}/subdomains?relationships=resolutions`;
+    const subdomain = await fetch(urlBase, {
+      method: "GET",
+      headers: {
+        "x-apikey": API_KEY,
+      },
+    }).then((res: any) => res.json());
+    const count = subdomain.meta.count;
+    let pages;
+    let SUBDOMAINS: any = [];
+    if (count >= 10) {
+      pages = Math.ceil(count / 10);
+      for (let i = 0; i < pages; i++) {
+        //mostrando resultados de una pagina scaneada
+        let onePage = await fetch(urlBase, {
+          method: "GET",
+          headers: {
+            "x-apikey": API_KEY,
+          },
+        }).then((res: any) => res.json());
+
+        let results = await this.convert(onePage.data);
+
+        if (
+          onePage.links.next !== null ||
+          onePage.links.next !== undefined ||
+          onePage.links.next !== "" ||
+          onePage.links.next !== " "
+        ) {
+          urlBase = onePage.links.next;
+        } else {
+          urlBase = "";
+        }
+        // unir los SUBDOMAINS
+        SUBDOMAINS = [...SUBDOMAINS, ...results];
+      }
+    } else {
+      SUBDOMAINS = this.convert(subdomain.data);
+    }
+    return SUBDOMAINS;
+  }
+  async convert(data: any) {
+    let domain = [];
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i].id;
+      domain.push({ subdomain: element });
+    }
+    return domain;
+  }
+}
+export default new VirusTotalClass();
--- src/resolvers/scanSubDomains.ts
+++ src/resolvers/scanSubDomains.ts
@@ -1,21 +1,26 @@
 import { Query, Resolver, Mutation, Arg } from "type-graphql";
 import SubfinderClass from "../class/SubFinderClass";
 import Subquest from "../class/Subquest";
+import VirusTotal from "../class/VirusTotal";
 
 import { Domain } from "../types/Domain";
 
 export class scanSubDomainsResolver {
   @Query(() => [Domain])
-  async scanByCloudflare(@Arg("domain", () => String) domain: string) {
-    return await SubfinderClass.scan(domain);
+  async scanByCloudflare(@Arg("host", () => String) host: string) {
+    return await SubfinderClass.scan(host);
   }
   @Query(() => [Domain])
   async scanBySubquest(
-    @Arg("domain", () => String) domain: string,
+    @Arg("host", () => String) host: string,
     @Arg("dictionary", () => Number) dictionary: number,
     @Arg("dnsServer", () => String, { nullable: true }) dnsServer?: string
   ) {
     dnsServer === undefined || dnsServer === null ? "1.1.1.1" : dnsServer;
-    return await Subquest.scan(domain, dictionary, dnsServer);
+    return await Subquest.scan(host, dictionary, dnsServer);
+  }
+  @Query(() => [Domain])
+  async scanByVirusTotal(@Arg("host", () => String) host: string) {
+    return await VirusTotal.scan(host);
   }
 }
