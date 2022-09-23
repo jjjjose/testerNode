2c028c69205e00926ab9a292bd1a77870f47fbb3
jjjjose <jjjjoseignacio@gmail.com>
2022-09-22 23:44:32 -0400

    mejora para modo mobil

--- frontend/src/components/IndexPage/tFooter.vue
+++ frontend/src/components/IndexPage/tFooter.vue
@@ -17,7 +17,7 @@
             bg-color="cyan-1"
             label-color="cyan-7"
             class="col-7"
-            @keyup.enter="searchSubdomains"
+            @keyup.enter="menu = true"
           >
             <template v-slot:prepend>
               <q-icon name="search" color="cyan-7" />
@@ -31,15 +31,69 @@
             class="col"
             align="around"
             icon-right="chevron_right"
-            @click="searchSubdomains"
+            @click="menu = true"
           />
         </div>
       </div>
     </q-toolbar>
+    <q-dialog v-model="menu" persistent>
+      <q-card>
+        <q-bar>
+          <div>Elige una opcion de Scan</div>
+
+          <q-space />
+
+          <q-btn dense flat icon="close" v-close-popup>
+            <q-tooltip>Close</q-tooltip>
+          </q-btn>
+        </q-bar>
+
+        <q-card-section class="column">
+          Scan normal, se realiza con apis públicas
+          <q-btn
+            push
+            color="primary"
+            label="Normal"
+            @click="searchSubdomains"
+          />
+        </q-card-section>
+
+        <q-card-section class="q-pt-none column">
+          <p>
+            Scan avanzado, se realiza un scan con Diccionario para buscar
+            posibles subdominios, usando BRUTE FORCE
+          </p>
+          <p>
+            para usar esta opcion debe seleccionar un diccionario de cantidad de
+            palabras
+          </p>
+
+          <div class="column q-gutter-sm">
+            <q-select
+              dense
+              outlined
+              v-model="dictionary"
+              :options="optionsDictionary"
+              label="Elije un diccionario"
+              class="col"
+              @update:model-value="funAdd"
+            />
+            <q-btn
+              :disable="disableBtnBF"
+              push
+              color="purple"
+              label="Brute Force"
+              class="col"
+              @click="searchBruteforce"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useGeneralStore } from "../../stores/general";
 import { useSubdomainsStore } from "../../stores/subdomains";
 import { useQuasar } from "quasar";
@@ -48,8 +102,13 @@ const subdomainsStore = useSubdomainsStore();
 const $q = useQuasar();
 const HOST: any = ref("");
 const generalStore = useGeneralStore();
+const menu = ref(false);
+const dictionary = ref("");
+const optionsDictionary = ref([50, 100, 250, 1000, 5000]);
+const disableBtnBF = ref(true);
 
 async function searchSubdomains() {
+  menu.value = false;
   showLoading();
   generalStore.changeLoading(true);
   await subdomainsStore
@@ -88,4 +147,39 @@ function showLoading() {
     message: "Estamos buscando subdominios para: \n " + HOST.value,
   });
 }
+function funAdd() {
+  console.log("add");
+}
+async function searchBruteforce() {
+  menu.value = false;
+  showLoading();
+  generalStore.changeLoading(true);
+  await subdomainsStore
+    .searchBruteForce(HOST.value, Number(dictionary.value))
+    .then((res) => {
+      $q.loading.hide();
+      generalStore.changeLoading(false);
+      showNotify(
+        "positive",
+        "BruteForce terminado con exito.",
+        "Se encontraron un total de: " + res + " subdominios"
+      );
+      subdomainsStore.changeCheckStatus(false);
+    })
+    .catch((err) => {
+      $q.loading.hide();
+      generalStore.changeLoading(false);
+      showNotify("negative", "Algo salio mal!, Intentalo nuevamente.", err);
+      subdomainsStore.changeCheckStatus(true);
+    });
+}
+watch(dictionary, (newDictionary) => {
+  if (
+    newDictionary !== "" ||
+    newDictionary !== null ||
+    newDictionary !== undefined
+  ) {
+    disableBtnBF.value = false;
+  }
+});
 </script>
